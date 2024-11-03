8e1e330acd2dad37f87a6e08385bb7aa88577c6c
SanchuCortes <cortesdominguezsantiago@gmail.com>
2024-11-03 10:28:09 +0100

    ToggleButton

---
+++ app/src/main/java/com/example/tareas/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.tareas;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ToggleButton toggleButton;
+    private ImageView imageView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        toggleButton = findViewById(R.id.toggleButton);
+        imageView = findViewById(R.id.imageView);
+
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    System.out.println("Activado");
+                    imageView.setVisibility(View.VISIBLE);
+                }else{
+                    System.out.println("Desactivado");
+                    imageView.setVisibility(View.GONE);
+                }
+            }
+        });
+
+    }
+}
