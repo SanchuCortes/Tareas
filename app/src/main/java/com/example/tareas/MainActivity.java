3202d731266c3af00dccbe54e3efef168258acf5
SanchuCortes <cortesdominguezsantiago@gmail.com>
2024-11-04 16:41:52 +0100

    ToggleButton

--- app/src/main/java/com/example/tareas/MainActivity.java
+++ app/src/main/java/com/example/tareas/MainActivity.java
@@ -1,53 +1,40 @@
 package com.example.tareas;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
-import android.widget.Switch;
-import android.widget.TextView;
 import android.widget.ToggleButton;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Switch switchButton;
+    private ToggleButton toggleButton;
     private ImageView imageView;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-       switchButton = findViewById(R.id.switch1);
+        toggleButton = findViewById(R.id.toggleButton);
         imageView = findViewById(R.id.imageView);
 
-        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+        toggleButton.setText("Mostrar Imagen");
+
+
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b){
-                    System.out.println("Activado");
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
                     imageView.setVisibility(View.VISIBLE);
-                }else{
-                    System.out.println("Desactivado");
+                    toggleButton.setText("Ocultar Imagen");
+                } else {
                     imageView.setVisibility(View.GONE);
+                    toggleButton.setText("Mostrar Imagen");
                 }
             }
         });
-
     }
 }
